678015e4690f65470061881b4f828d898f1bf9bb
Adarsh Yadav <136366968+adarshyadav444@users.noreply.github.com>
2026-01-22 10:52:51 +0530

    Simplify factorial program to use recursion only
    Removed implementations using loops and kept the recursive approach for calculating factorial.

--- Problem 1.py
+++ Problem 1.py
@@ -3,36 +3,16 @@
 # 2.   Returns the calculated factorial.
 # 3.   Calls the function with a sample number and prints the output.
 
-
-#Using While Loop
-# def factorial(n):
-#     fact = 1
-#     while n > 0:
-#         fact = fact * n
-#         n -= 1
-#     return fact
-
-# n = int(input("Please Enter Your Number: "))
-# print(f"Factorial of {n} is: {factorial(n)}")
-
-#Using For Loop
-# def factorial(n):
-#     fact = 1
-#     for i in range(1, n+1):
-#         fact = fact * i
-#     return fact
-# n = int(input("Please Enter Your Number: "))
-# print(f"Factorial of {n} is: {factorial(n)}")
-
 #Using Recursion 
-def factorial(n):
-    if n <= 1:
-        return 1
-    else:
-        return factorial(n-1) * n
+# def factorial(n):
+#     if n <= 1:
+#         return 1
+#     else:
+#         return factorial(n-1) * n
 
-n = int(input("Please Enter Your Number: "))
-print(f"Factorial of {n} is: {factorial(n)}")
+# n = int(input("Please Enter Your Number: "))
+# print(f"Factorial of {n} is: {factorial(n)}")
+
 
 
 
